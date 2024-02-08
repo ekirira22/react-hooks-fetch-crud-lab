f5b92628fcb8aa52315317927a2afa1f913a425e
Eric Maranga <ekirira22@gmail.com>
2024-02-08 10:03:52 +0300

    displayed questions successfully

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 function QuestionList() {
+    //Load list items when component loads
+  const API = "http://localhost:4000/questions"
+  const [allQuestions, setAllQuestions] = useState([])
+
+  useEffect(() => {
+    fetchQuestions()
+  },[])
+
+  async function fetchQuestions(){
+    const questions = await fetch(API)
+    const results = await questions.json()
+    // console.log(results)
+    setAllQuestions(results)
+  }
+
+  // console.log(allQuestions)
+  const displayQuestions = allQuestions.map((question) => {
+    return (
+      <li key={question.id}>
+        <form>
+          <label>{question.prompt}</label>
+          <select name="answers" id="answers">
+            {question.answers.map((answer) => {
+              return <option key={answer}>{answer}</option>
+            })}
+          </select>
+          <button>DELETE</button>
+        </form>
+      </li>
+    )
+  })
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{displayQuestions}</ul>
     </section>
   );
 }
